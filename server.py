9e579d0269faefc1fd730b5803c835c23785a614
Anne Bercilla <annebercilla@gmail.com>
2021-06-06 21:59:11 +0000

    Took out form from homepage.html to try to debug server issue

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-# """Server for civic app"""
+"""Server with all routes"""
 
-from flask import Flask, render_template, request, flash, session, redirect
+from flask import (Flask, render_template, request, flash, session, redirect)
 
 from model import connect_to_db
 import crud
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.secret_key = "DEV" #need to change this and add to secrets.sh
 app.jinja_env.undefined = StrictUndefined
 
-@app.route("/")
+@app.route('/')
 def index():
     """Return homepage"""
     #if user has already signed in, redirect to feed
@@ -21,18 +21,19 @@ def index():
     #     return redirect("/feed")
     # else:
     #     return 
-    return render_template("homepage.html")
+    return render_template('homepage.html')
 
-@app.route("/create-account", methods=['POST'])
+@app.route('/create-account', methods=['POST'])
 def create_account():
     """Create account for user"""
 
-    session['fname'] = request.form.get("fname")
+    session['fname'] = request.form.get('fname')
     
-    return render_template("/feed")
+    return render_template('/feed')
 
 
 if __name__ == "__main__":
-    #eventually we'll put connect_to_db here
+    
+    connect_to_db(app)
 
     app.run(host="0.0.0.0", debug=True)
