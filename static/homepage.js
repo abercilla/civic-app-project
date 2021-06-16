1eca7274e258517d471e1eaa1cec75e2970e56a7
Anne Bercilla <annebercilla@gmail.com>
2021-06-16 19:49:26 +0000

    Got search keyword to stick in localStorage

--- static/homepage.js
+++ static/homepage.js
@@ -12,61 +12,72 @@ alert("js is connected!");
 
 
 
-// DISPLAY RELEVANT EVENTS
+//----------- DISPLAY RELEVANT EVENTS --------------------//
 // events that fit the current user's saved preferences will be returned as (data) from get request
 //... and we'll append each event to list element <li>
 
-let url = "/saved-filter.json";
-let i = 0;
+function filterHomepage (evt) {
 
-$('#saved_filters').on('click', (evt) => {
-    evt.preventDefault();
-    console.log("On submit is working!")
-    $.get(url, (data) => {
-        $('#event-list').empty();
-        let events = data;
-        for (const event of events) {
-            console.log(events[i]["event_id"]);
-            const eventAdd = "<li><a href = 'events/" + events[i]["event_id"] + "'>" + events[i]["name"] + "</a></li>";
-            $('#event-list').append(eventAdd);
-            i = i + 1;
-        }     
-    })
-});
+    let url = "/saved-filter.json";
+    let i = 0;
+
+        evt.preventDefault();
+        console.log("On submit is working!")
+
+        $.get(url, (data) => {
+            $('#event-list').empty();
+            let events = data;
+            for (const event of events) {
+                console.log(events[i]["event_id"]);
+                const eventAdd = "<li><a href = 'events/" + events[i]["event_id"] + "'>" + events[i]["name"] + "</a></li>";
+                $('#event-list').append(eventAdd);
+                i = i + 1;
+            }     
+        });
+}
+
+$('#saved_filters').on('click', filterHomepage);
 
 
-//-------
-// Make user's search criteria STICKY
+
+// --------------Make user's search criteria STICKY----------//
+
 //this will listen for all checkboxes on homepage.html
 // var i is a number of checkboxes
 var j, boxes = document.querySelectorAll('input[type="checkbox"]');
-//var keyword = document.querySelectorAll('input[type="text"]');
+//grab input from any text box from this file
+var keyword = document.querySelector('#keyword');
 
-//create items in localStorage to store user's input 
+//create dict items in localStorage to store user's input 
 function save() {
+    
+    //grab what user entered in search box and store in localStorage 
+    //...as {key = 'keyword': value = keyword.value}
+    localStorage.setItem('keyword', keyword.value);
+    
     //loop over all the checkboxes 
     for (j = 0; j < boxes.length; j++) {
         //create dict in localStorage where {key = 'category': value = 'true/false'}
         localStorage.setItem(boxes[j].value, boxes[j].checked); 
-    }
-    //create dict in localStorage where {key = 'keyword': value= user input}
-    //localStorage.setItem(keyword, keyword.value);
+    } 
 }
 
 //load what the user entered on the page from localStorage 
 function load_() {
-    //loop over checkboxes again to check for values 
+    
+    //refill the HTML element with what was stored in localStorage
+    keyword.value = localStorage.getItem('keyword');
+
+    //loop over checkboxes again to check for values
     for (j = 0; j < boxes.length; j++) {
         //set the dict value for boxes to whether or not they're checked (??) 
         // snippet from: https://jsfiddle.net/sQuEy/4/
         boxes[j].checked = localStorage.getItem(boxes[j].value) === 'true' ? true:false;
     }
-    //keep keyword textbox entered on page
-    //fill in whatever keyword is stored in localStorage 
-    //localStorage.getItem(keyword.value === 
 }
 
-//clear filters via localStorage
+// clear filters via localStorage
+// BUG --- Clear Filters should remove filters and have all events show up again
 const clear = () => {
     localStorage.clear();
     location.reload();
@@ -89,8 +100,11 @@ const saveUserFilter = (evt) => {
             console.log(response)
         });
 }
-
+//when "Apply Filters" is clicked
 $('#save_to_prefs').on("click", saveUserFilter);
+
+//when "Clear Filter" is clicked
 $('#clear-button').on("click", clear);
+
 $(document).ready(load_());
 
