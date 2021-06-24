73eec0426f468786c12ef0dcaacd1185e2e09279
Anne Bercilla <annebercilla@gmail.com>
2021-06-24 20:49:33 +0000

    Started implementing Google Maps API

---
+++ static/create-event.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// attached to create-event.html
+
+alert("create-event.js is connected!");
+
+//when you click "Upload Image"
+var loadFile = function(event) {
+    //grab 
+    var image = document.getElementById('output');
+    //create a URL out of the uploaded image file
+    console.log(image);
+    
+    image.src = URL.createObjectURL(event.target.files[0]);
+    
+};
+ 
---
+++ static/event-details.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// attached to create-event.html
+
+alert("event-details.js is connected!");
+
+function loadImage(src){
+    request.onload = 
+    
+    //when you click "Upload Image"
+var loadFile = function(event) {
+    //grab 
+    var image = document.getElementById('output');
+    //create a URL out of the uploaded image file
+    console.log(image);
+    
+    image.src = URL.createObjectURL(event.target.files[0]);
+    
+};
+ 
